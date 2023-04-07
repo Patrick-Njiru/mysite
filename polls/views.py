1bc4c4608001a4352f83dd4105ced5a93264ad27
Patrick-Njiru <patricknjiru93@gmail.com>
2023-04-07 17:01:28 +0300

    Add static files and fix bugs.

--- polls/views.py
+++ polls/views.py
@@ -30,7 +30,7 @@ class DetailView(generic.DetailView):
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'polls/detail.html'
+    template_name = 'polls/results.html'
     
 
 def vote(request, question_id):
