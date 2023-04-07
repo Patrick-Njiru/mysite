8bf18d9c7da3f7c0719386e67eeaf7af6bd93b4e
Patrick-Njiru <patricknjiru93@gmail.com>
2023-04-07 19:32:35 +0300

    Add and configure the debug_toolbar app

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,8 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls'
+    'polls',
+    'debug_toolbar',
 ]
+
+""" 
+The order of MIDDLEWARE is important. 
+You should include the Debug Toolbar middleware as early as possible in the list. 
+However, it must come after any other middleware that encodes the response’s content,
+such as GZipMiddleware. 
+"""
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -126,3 +135,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Internal Ips
+INTERNAL_IPS = [
+    # ...
+    '127.0.0.1',
+    # ...
+]
