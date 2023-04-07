bae6d66d8489cb4089d271bdf2441b26436a3452
Patrick-Njiru <patricknjiru93@gmail.com>
2023-04-07 18:36:00 +0300

    Modify the admin's page.

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-# Register your models here.
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    """ model to add choice fields when creating or modifying a question """
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    """ Change the appearance of the questions form or page. """
+    fieldsets = [
+        ("Date infromation", {"fields": ["pub_date"]}),
+        ("Question", {"fields": ["question_text"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["pk", "question_text", "pub_date", "was_published_recently"]
+    """ Add method to filter by and maybe editable cells in the table by the two methods below. Try other methods."""
+    list_filter = ['pub_date']
+    # list_editable = ['question_text']
+    search_fields = ["question_text"]
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+from django.contrib import admin
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    """ the string question and date published inside the parenthesis are used in the admins page in place of the variable names when provided """
+    question_text = models.CharField("question", max_length=200)
     pub_date = models.DateTimeField("date published")
 
     # for convenience when dealing with interactvie prompts
     def __str__(self):
         return self.question_text
+    
+    @admin.display(
+            boolean=True,
+            ordering="pub_date",
+            description="Published recently",
+    )
     
     def was_published_recently(self):
         now = timezone.now()
